7c4fd30c46532569ad65ef877978b16f733592f4
ANDRES LOPEZ MARTINEZ <andres.lopezm@elektra.com.mx>
2025-08-11 15:40:17 -0600

    Agrego estilos personalizados en Boton

--- src/components/Boton/Boton.tsx
+++ src/components/Boton/Boton.tsx
@@ -3,26 +3,31 @@ import React from 'react';
 import './Boton.scss';
 
 interface Props {
+  className?: string;
   etiqueta?: string;
   onClick?(): void;
   submit?: boolean;
   deshabilitado?: boolean;
-  estilo?: number;
+  variacion?: number;
+  estilo?: object;
 }
 
 const Boton = ({
+  className = '',
   etiqueta = 'Continuar',
   onClick = () => {},
   submit = false,
   deshabilitado = false,
-  estilo = 1,
+  variacion = 1,
+  estilo,
 }: Props) => {
   return (
     <button
-      className={`boton estilo-${estilo}`}
+      className={`boton estilo-${variacion} ${className}`}
       type={submit ? 'submit' : 'button'}
       onClick={onClick}
       disabled={deshabilitado}
+      style={estilo}
     >
       {etiqueta}
     </button>
